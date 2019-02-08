ea8ac22945a140ac0d016ea7b05ae2a41b8e6cb6
Richard Batista <36830488+blue-stalfos@users.noreply.github.com>
2019-02-08 15:32:08 -0500

    Completed lesson

---
+++ src/com/richardbatista/Main.java
@@ -0,0 +1,24 @@
+package com.richardbatista;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Player player = new Player();
+//
+//        player.name = "Richard";
+//        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health: " + player.healthRemaining());
+//
+//        damage = 11;
+//        player.health = 200;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health: " + player.healthRemaining());
+
+        EnhancedPlayer player = new EnhancedPlayer("Elias", 200, "Sword");
+        System.out.println("Initial health is: " + player.getHealth());
+    }
+}
